5d644a29caddf9569b686c46a2cd478b99c22265
admin <admin@admin-PC>
2019-04-25 13:54:59 +0800

    1

---
+++ src/test1/BasicDataConsts.java
@@ -0,0 +1,21 @@
+package test1;
+
+/**
+ * <p>Title: V2快�?�开发框�?</p>
+ * <p>Description: 基础数据常量</p>
+ * <p>Copyright: Copyright (c) 2015</p>
+ * <p>Company:武汉通用电子有限公司</p>
+ * @author 江山
+ * @version 1.0
+ */
+public class BasicDataConsts {
+	/** 字典项代�?: 招标类型--施工 **/
+	public static final String ZBLX_SG_ZDXDM = "003";
+
+	/** 字典项代�?: 招标类型--监理 **/
+	public static final String ZBLX_JL_ZDXDM = "004";
+	/**
+	 * 默认管理部门：武汉市建设工程交易中心
+	 */
+	public static final String DEFAULT_COMPETENT_DEPART = "01";		
+}
